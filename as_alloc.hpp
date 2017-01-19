85bc0a8df1fa28059ba21726202a455b6ee07dfa
Paul Keir <paul.keir@uws.ac.uk>
2017-01-19 23:56:18 +0000

    Almost there, apart from a little int issue.

--- as_alloc.hpp
+++ as_alloc.hpp
@@ -1,35 +1,37 @@
 #ifndef __AS_ALLOCATOR_HPP__
 #define __AS_ALLOCATOR_HPP__
 
+#include <iostream>
+
 template <typename T>
-struct as-alloc {
+struct as_alloc {
 
   using value_type = typename T::type;
 
-  as-alloc () { cout << "ctor\n"; }
+  as_alloc() { std::cout << "ctor\n"; }
 
-  template <typename U> as-alloc(const as-alloc<U> &other) {
-    cout << "copy ctor\n";
+  template <typename U> as_alloc(const as_alloc<U> &other) {
+    std::cout << "copy ctor\n";
   }
 
   value_type *allocate(std::size_t n) {
-    cout << "allocate\n";
+    std::cout << "allocate\n";
     return new value_type[n];
   }
 
   void deallocate(value_type *p, std::size_t n) {
-    cout << "deallocate\n";
+    std::cout << "deallocate\n";
     delete [] p;
   }
 };
 
 template <typename T, typename U>
-bool operator==(const as-alloc<T>&a, const as-alloc<U>&b) {
+bool operator==(const as_alloc<T>&a, const as_alloc<U>&b) {
   return true; // possibly wrong
 }
 
 template <typename T, typename U>
-bool operator!=(const as-alloc<T>&a, const as-alloc<U>&b) {
+bool operator!=(const as_alloc<T>&a, const as_alloc<U>&b) {
   return !a==b;
 }
 
