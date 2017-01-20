f5a386a5464f7b97da5eb4a6c334715cf451fd3a
Paul Keir <paul.keir@uws.ac.uk>
2017-01-20 22:00:03 +0000

    Added tag_wrap class which may use an i_seq as its second argument.

--- example_wrap.cpp
+++ example_wrap.cpp
@@ -1,12 +1,5 @@
 #include <iostream>
-#include "as_trait.hpp"
-
-template <typename T>
-struct wrap {
-  wrap(T x) : x(x) {}
-  operator T() { return x; }
-  T x;
-};
+#include "tag_wrap.hpp"
 
 void grab(int x) {}
 struct Foo {};
@@ -15,16 +8,16 @@ void jim(Foo o) {}
 int main(int argc, char *argv[])
 {
   int i = 123;
-  wrap w(9);
+  tag_wrap w(9);
   grab(w);
   std::cout << w << '\n';
-  wrap w2(9.3);
+  tag_wrap w2(9.3);
   std::cout << w2 << '\n';
-  wrap p(&i);
+  tag_wrap p(&i);
   std::cout << *p << '\n';
 
   Foo o;
-  wrap w3(o);
+  tag_wrap w3(o);
   jim(w3);
 
   std::cout << w << '\n';
@@ -32,7 +25,7 @@ int main(int argc, char *argv[])
   std::cout << w << '\n';
   double bob[2][3] = {1,2,3,4,5,6};
   std::cout << bob[1][2] << '\n';
-  wrap w4(bob);
+  tag_wrap w4(bob);
   std::cout << w4[1][2] << '\n';
   
   return 0;
