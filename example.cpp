35cf8856eb23c0d0549cfa5eed0e8ea6601e0f9a
Paul Keir <paul.keir@uws.ac.uk>
2017-01-20 15:13:40 +0000

    Some work.

--- example.cpp
+++ example.cpp
@@ -23,13 +23,17 @@ int main(int argc, char *argv[])
   std::vector<int> v1 = {3,2,1};
   std::vector<int> v2{3,2,1};
   std::vector<int> v3(3,2); // n.b. Three 2s
+
   print(v1,v2,v3);
 
   {
     std::vector<int,simple_alloc<int>> v4{3,2,1};
+    print(v1,v2,v3);
   }
+  std::cout << '\n';
   {
     std::vector<int,id_alloc<id<int>>> v5{3,2,1};
+    print(v1,v2,v3);
   }
   return 0;
 }
