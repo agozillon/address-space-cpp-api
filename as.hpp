874b2e3b785ef50bbd9c8ad57793500e55c80042
Paul Keir <paul.keir@uws.ac.uk>
2017-01-20 18:22:50 +0000

    Working on traits.

---
+++ as.hpp
@@ -0,0 +1,9 @@
+#ifndef __AS_HPP__
+#define __AS_HPP__
+
+template <typename T, typename I>
+struct as {
+  T x; // by value or by reference?
+};
+
+#endif // __AS_HPP__
---
+++ basic.hpp
@@ -0,0 +1,6 @@
+#ifndef __BASIC_HPP__
+#define __BASIC_HPP__
+
+using uint = unsigned;
+
+#endif // __BASIC_HPP__
