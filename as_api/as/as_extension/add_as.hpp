1e882dde600c8facd959916c910e2648726133d1
Paul Keir <paul.keir@uws.ac.uk>
2017-04-21 12:02:51 +0100

    Reorganised, and completed a simple example which prints the address space.

---
+++ as_api/as/as_extension/add_as.hpp
@@ -0,0 +1,12 @@
+#ifndef __ADD_AS_HPP__
+#define __ADD_AS_HPP__
+
+template <typename T, unsigned Nv>
+struct add_as {
+  using type = T __attribute__((ext_address_space(Nv)));
+};
+
+template <typename T, unsigned Nv>
+using add_as_t = typename add_as<T,Nv>::type;
+
+#endif // __ADD_AS_HPP__
