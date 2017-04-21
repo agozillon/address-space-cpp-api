1e221af0f2d76d02f7dec412b0d56148ad6c0e64
Paul Keir <pkeir@users.noreply.github.com>
2017-04-21 12:02:51 +0100

    Reorganised, and completed a simple example which prints the address space.

--- as_api/as/as_val/add_as.hpp
+++ as_api/as/as_val/add_as.hpp
@@ -3,19 +3,6 @@
 
 #include "as_val.hpp"
 
-// set, not add; certainly not like add_pointer; but even add_const...a user
-// will not expect add_const<int const> to enable an "int const const".
-// No, for a user of stock GCC, add_as implies a change; fair enough.
-
-#if __has_attribute(ext_address_space)
-
-template <typename T, unsigned Nv>
-struct add_as {
-  using type = T __attribute__((ext_address_space(Nv)));
-};
-
-#else
-
 template <typename T, unsigned Nv>
 struct add_as {
   using type = as_val<T,0,Nv>;
@@ -25,8 +12,6 @@ template <typename T, unsigned Np, unsigned Nv, unsigned Nv_>
 struct add_as<as_val<T,Np,Nv>, Nv_> {
   using type = as_val<T,Np,Nv_>;
 };
-
-#endif
 
 template <typename T, unsigned Nv>
 using add_as_t = typename add_as<T,Nv>::type;
---
+++ as_api/as/as_val/get_as.hpp
@@ -0,0 +1,24 @@
+#ifndef __GET_AS_HPP__
+#define __GET_AS_HPP__
+
+#include "add_as.hpp"
+
+template <typename>
+  struct get_as;
+
+template <typename T, unsigned Np, unsigned Nv>
+struct get_as<as_val<T,Np,Nv>> {
+  static const unsigned value = Nv;
+};
+
+template <typename T>
+inline constexpr auto get_as_v = get_as<T>::value;
+
+namespace get_as_hpp_tests {
+
+static_assert(1  == get_as_v<add_as_t<float,1>>);
+static_assert(42 == get_as_v<add_as_t<float,42>>);
+
+} // namespace get_as_hpp_tests
+
+#endif // __GET_AS_HPP__
