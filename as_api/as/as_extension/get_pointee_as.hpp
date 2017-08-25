c4f2847623736869923fb45d145f2d35617c0c06
Andrew Gozillon <andrew.gozillon@uws.ac.uk>
2017-08-25 09:45:28 +0100

    Adding new get_pointee_as template and removing a couple of excess comments

---
+++ as_api/as/as_extension/get_pointee_as.hpp
@@ -0,0 +1,32 @@
+#ifndef __GET_POINTEE_AS_HPP__
+#define __GET_POINTEE_AS_HPP__
+
+#include <type_traits>
+#include "add_pointee_as.hpp"
+
+template<typename T, unsigned Nv = 0>
+struct get_pointee_as {
+   static const unsigned value = Nv;
+};
+
+template <typename T, unsigned Nv>
+struct get_pointee_as<T __attribute__((ext_address_space(Nv))) *> {
+  static const unsigned value = Nv;
+};
+
+template <typename T>
+inline constexpr auto get_pointee_as_v = get_pointee_as<T>::value;
+
+/* -------------------------------------------------------------------------- */
+
+namespace get_pointee_as_hpp_tests {
+
+static_assert(0  == get_pointee_as_v<float**>, "");
+static_assert(7  == get_pointee_as_v<add_pointee_as_t<double**,7>>, "");
+static_assert(42 == get_pointee_as_v<add_pointee_as_t<int**,42>>, "");
+static_assert(0 == get_pointee_as_v<add_pointee_as_t<float*,42>*>, "");
+static_assert(1  == get_pointee_as_v<float * __attribute__((ext_address_space(1))) *>);
+
+} // namespace get_pointee_as_hpp_tests
+
+#endif // __GET_POINTEE_AS_HPP__
